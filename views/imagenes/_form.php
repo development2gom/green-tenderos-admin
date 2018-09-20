83e2527f6be7b9bdeb2e54b6ac8f9f4827a1a778
Damián <damian2gom@gmail.com>
2018-09-20 13:47:19 -0500

    Crear - Datos/Imagen/vídeo

--- views/imagenes/_form.php
+++ views/imagenes/_form.php
@@ -10,26 +10,35 @@ use yii\helpers\ArrayHelper;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="ent-imagenes-form">
+<div class="page-datos-cont-actions">
 
-    <?php $form = ActiveForm::begin(); ?>
-<div class="row">
-    <div class="col-6">
-        <?= $form->field($model, 'id_concurso')->dropDownList(ArrayHelper::map(CatConcurso::find()->where(['b_habilitado'=>1])->OrderBy('txt_nombre ASC')->all(), 'id_concurso', 'txt_nombre'), ['prompt'=>'Seleccionar concurso']) ?>   
-    </div>
-    <div class="col-6">
-        <?= $form->field($model, 'txt_nombre')->textInput(['maxlength' => true]) ?>
-    </div>
-    <div class="col-6">
-        <?= $form->field($model, 'fileUpload')->fileInput(['accept'=>'image/*']) ?>
-    </div>
-   
-        
+    <button type="button" class="btn btn-success btn-outline">
+        <i class="icon ion-ios-color-wand" aria-hidden="true"></i>
+    </button>
+    <button type="button" class="btn btn-danger btn-outline">
+        <i class="icon ion-ios-trash" aria-hidden="true"></i>
+    </button>
+</div>
+
+<?php $form = ActiveForm::begin(); ?>
+
+    <div class="row">
 
         <div class="col-12">
-            <?= Html::submitButton('GUARDAR', ['class' => 'btn btn-success']) ?>
+            <?= $form->field($model, 'id_concurso')->dropDownList(ArrayHelper::map(CatConcurso::find()->where(['b_habilitado'=>1])->OrderBy('txt_nombre ASC')->all(), 'id_concurso', 'txt_nombre'), ['prompt'=>'Seleccionar concurso']) ?>   
         </div>
-</div>
-    <?php ActiveForm::end(); ?>
+        <div class="col-12">
+            <?= $form->field($model, 'txt_nombre')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class="col-12">
+            <?= $form->field($model, 'fileUpload')->fileInput(['accept'=>'image/*']) ?>
+            <input type="file" class="js-input-file" id="input-file-now" data-plugin="dropify" data-allowed-file-extensions="" data-default-file="" />
+        </div>
 
-</div>
+        <div class="col-12 col-actions">
+            <?= Html::submitButton('GUARDAR', ['class' => 'btn btn-primary']) ?>
+        </div>
+
+    </div>
+
+<?php ActiveForm::end(); ?>
--- views/videos/_form.php
+++ views/videos/_form.php
@@ -10,25 +10,36 @@ use app\models\CatConcurso;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="ent-videos-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-        <div class="row">
-        <div class="col-6">
-        <?= $form->field($model, 'id_concurso')->dropDownList(ArrayHelper::map(CatConcurso::find()->where(['b_habilitado'=>1])->OrderBy('txt_nombre ASC')->all(), 'id_concurso', 'txt_nombre'), ['prompt'=>'Seleccionar concurso']) ?>   
+<div class="page-datos-cont-actions">
+
+    <button type="button" class="btn btn-success btn-outline">
+        <i class="icon ion-ios-color-wand" aria-hidden="true"></i>
+    </button>
+    <button type="button" class="btn btn-danger btn-outline">
+        <i class="icon ion-ios-trash" aria-hidden="true"></i>
+    </button>
+</div>
+
+<?php $form = ActiveForm::begin(); ?>
+
+    <div class="row">
+        <div class="col-12">
+            <?= $form->field($model, 'id_concurso')->dropDownList(ArrayHelper::map(CatConcurso::find()->where(['b_habilitado'=>1])->OrderBy('txt_nombre ASC')->all(), 'id_concurso', 'txt_nombre'), ['prompt'=>'Seleccionar concurso']) ?>   
         </div>
-        <div class="col-6">
+
+        <div class="col-12">
             <?= $form->field($model, 'txt_nombre')->textInput(['maxlength' => true]) ?>
         </div>
-        <div class="col-6">
-            <?= $form->field($model, 'fileUpload')->fileInput() ?>
-        </div>
-        
-        
-        <div class="col-12">
-            <?= Html::submitButton('GUARDAR', ['class' => 'btn btn-success']) ?>
-        </div>
-        </div>
-    <?php ActiveForm::end(); ?>
 
-</div>
+        <div class="col-12">
+            <?= $form->field($model, 'fileUpload')->fileInput() ?>
+            <input type="file" class="js-input-file" id="input-file-now" data-plugin="dropify" data-allowed-file-extensions="" data-default-file="" />
+        </div>
+        
+        <div class="col-12 col-actions">
+            <?= Html::submitButton('GUARDAR', ['class' => 'btn btn-primary']) ?>
+        </div>
+    </div>
+
+<?php ActiveForm::end(); ?>
