4d6e682ab95115dd397c4b902d9f8ffc6e917107
Damián <damian2gom@gmail.com>
2018-09-21 12:41:46 -0500

    Login

--- modules/ModUsuarios/views/manager/login.php
+++ modules/ModUsuarios/views/manager/login.php
@@ -19,7 +19,7 @@ $this->params['classBody'] = "page-login-v3 layout-full login-page";
 	<div class="panel-body">
 		
 		<h2>
-            <img src="<?=Url::base()?>/webAssets/images/logo-latingal-boutique.png" alt="">
+            <img src="<?=Url::base()?>/webAssets/images/logo-gepp-blue.png" alt="">
         </h2>
 
 
@@ -50,7 +50,7 @@ $this->params['classBody'] = "page-login-v3 layout-full login-page";
 		</div>
 
 		<div class="form-group form-group-actions">
-			<?= Html::submitButton('<span class="ladda-label">Ingresar</span>', ["data-style" => "zoom-in", 'class' => 'btn btn-secondary btn-block btn-lg mt-20 ladda-button', 'name' => 'login-button']); ?>
+			<?= Html::submitButton('<span class="ladda-label">INGRESAR</span>', ["data-style" => "zoom-in", 'class' => 'btn btn-secondary btn-block btn-lg mt-20 ladda-button', 'name' => 'login-button']); ?>
 		</div>
 
 		<!--<div class="form-group necesito-cuenta">
